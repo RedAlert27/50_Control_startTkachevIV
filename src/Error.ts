12ad749e3a32612a334ccef5ff4c664a84601b22
Igor <igortkachev94@gmail.com>
2026-01-30 21:52:24 +0700

    Update Error.ts

--- src/Error.ts
+++ src/Error.ts
@@ -1,2 +1,8 @@
-export class GameError extends Error{}
-export const SymError = new GameError("Wromg symbol")
+export class GameError extends Error {
+    constructor(message: string) {
+        super(message)
+        this.name = "GameError"
+    }
+}
+
+export const SymError = new GameError("Wrong symbol")
