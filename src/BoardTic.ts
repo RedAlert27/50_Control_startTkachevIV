02225b37cbbcdd873e99d4333eab0e7ac6a89bb5
Igor <igortkachev94@gmail.com>
2026-01-30 21:52:06 +0700

    Update BoardTic.ts

--- src/BoardTic.ts
+++ src/BoardTic.ts
@@ -1,27 +1,33 @@
 import { Board, BoardParam } from "./Board"
-import { Sym } from "./Sym"
 import { SymTic } from "./SymTic"
 
 export class BoardTic extends Board<GameTic> {
-
     constructor(
         str: string | SymTic[] = "_________",
         init: boolean = true
     ) {
-        // TODO
-        // Если длина str!=9, то используется строка по умолчанию
-        // Вызывает родительский конструктор,
-        //  если init, то дополнительно инициализируются
-        //  статические поля класса
-        super({} as Sym<GameTic>[])
+        // Преобразование входных данных
+        let cells: SymTic[]
+        if (Array.isArray(str)) {
+            cells = str
+        } else {
+            if (str.length !== 9) {
+                str = "_________"
+            }
+            cells = SymTic.StringToSyms<GameTic>(str)
+        }
+
+        // Инициализация статических полей
+        if (init) {
+            ;(BoardTic as any).row = BoardTicParam.row
+            ;(BoardTic as any).col = BoardTicParam.col
+        }
+
+        super(cells, BoardTicParam.row, BoardTicParam.col)
     }
 
     clone(): BoardTic {
-        // TODO
-        // Функция должна вернуть копию объекта
-        // Если init, то дополнительно инициализируются
-        //  статические поля класса
-        return this
+        return new BoardTic([...this.cells], false)
     }
 
     private getLineChar(line: number[]): string[] {
@@ -33,29 +39,30 @@ export class BoardTic extends Board<GameTic> {
     }
 
     private static winPos = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6]
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
     ]
 
-    private checkWin() {
-        // TODO
-        // Если имеется комбинация из трех одинаковых символов "X" или "0" 
-        //  в линию - возвращает этот символ
-        // Иначе возвращает символ "_"
+    private checkWin(): string {
+        for (const line of BoardTic.winPos) {
+            const [a, b, c] = this.getLineChar(line)
+            if (a !== "_" && a === b && b === c) {
+                return a
+            }
+        }
         return "_"
     }
 
     override status(): string {
-        // TODO
-        // возвращает либо строку с результатом игры, либо, 
-        //   если игра не закончена, вызывает status родителя.
-        return ""
+        const winner = this.checkWin()
+        if (winner !== "_") {
+            return `Победил: ${winner}`
+        }
+        if (this.isFill()) {
+            return "Ничья!"
+        }
+        return super.status()
     }
 }
 
