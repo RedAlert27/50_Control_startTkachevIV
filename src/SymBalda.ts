196d9470580368623be91dddf5ed0d27f7fcec65
Igor <igortkachev94@gmail.com>
2026-01-30 21:57:22 +0700

    Update SymBalda.ts

--- src/SymBalda.ts
+++ src/SymBalda.ts
@@ -1,3 +1,11 @@
 import { Sym } from "./Sym"
 
-export class SymTic extends Sym<GameBalda> {}
+export class SymBalda extends Sym<GameBalda> {
+    override checkSym(sym: string): boolean {
+        if (!super.checkSym(sym)) {
+            return false
+        }
+        // Для балды допустимы русские буквы и пробел
+        return sym === " " || (sym.length === 1 && /[а-яА-ЯёЁ]/.test(sym))
+    }
+}
